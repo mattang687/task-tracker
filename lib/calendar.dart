20cd4fd9e382b3b3669bd79e0a0c04d9452a02f7
Matt Tang <mattang687@gmail.com>
2019-07-31 15:36:35 -0400

    streamlined calendar

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -8,18 +8,34 @@ class CalendarWidget extends StatefulWidget {
   }
 }
 
-class CalendarWidgetState extends State<CalendarWidget> {
+class CalendarWidgetState extends State<CalendarWidget> with TickerProviderStateMixin {
   CalendarController _calendarController;
-  
+  AnimationController _animationController;
+  Map<DateTime, List> _events;
+  List _selectedEvents;
+
   @override
   void initState() {
     super.initState();
     _calendarController = CalendarController();
+
+    DateTime _selectedDay = DateTime.now();
+
+    _events = {};
+    _selectedEvents = _events[_selectedDay] ?? [];
+
+    AnimationController _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+
+    _animationController.forward();
   }
 
   @override
   void dispose() {
     _calendarController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -27,6 +43,28 @@ class CalendarWidgetState extends State<CalendarWidget> {
   Widget build(BuildContext context) {
     return TableCalendar(
       calendarController: _calendarController,
+      events: _events,
+      headerVisible: false,
+      initialCalendarFormat: CalendarFormat.week,
+      formatAnimation: FormatAnimation.slide,
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      availableGestures: AvailableGestures.all,
+      availableCalendarFormats: const {
+        CalendarFormat.month: 'Month',
+        CalendarFormat.week: 'Week',
+      },
+      calendarStyle: CalendarStyle(
+        outsideDaysVisible: false,
+        weekendStyle: TextStyle().copyWith(color: Colors.blue[800]),
+        holidayStyle: TextStyle().copyWith(color: Colors.blue[800]),
+      ),
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekendStyle: TextStyle().copyWith(color: Colors.blue[600]),
+      ),
+      onVisibleDaysChanged: _onVisibleDaysChanged,
     );
+  }
+  _onVisibleDaysChanged(DateTime first, DateTime last, CalendarFormat format) {
+    // TODO: send date to appbar
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,6 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Tracker'),
+      ),
       body: CalendarWidget()
     );
   }
